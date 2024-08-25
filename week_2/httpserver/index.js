8b5e43fb63e7a7f7717e045ba1b96b558c0d60ec
Gauravthorat30 <162902777+Gauravthorat30@users.noreply.github.com>
2024-08-25 20:12:53 +0000

    http

--- week_2/httpserver/index.js
+++ week_2/httpserver/index.js
@@ -1,9 +1,35 @@
 const express = require("express");
+const bodyParser = require("body-parser"); 
 const port = 3000;
 const app = express();
+app.use(bodyParser.json());
 
-app.get("/" , function(req , res){
-    res.send("hello world")
+// app.get("/asd" , function(req , res){
+//     res.send("hello world")
+//     console.log(req.headers)
+//     const data = req.headers
+//     res.send(data);
+// })
+
+// app.post("/conversation" , function(req , res){
+
+//     console.log(req.headers["authorization"])
+//     res.send({
+//         name : "gaurav",
+//         age : 18
+//     });
+// })
+
+
+app.post("/" , function(req , res){
+    
+    console.log(req.body);
+    console.log("hiii")
+    res.send(req.body)
 })
 
-app.listen(port);
+
+
+app.listen(port , function(){
+    console.log("port started")
+});
